04d8a13fbb3e333b8b217a027509e5b4d7d6d5b9
Mor Twito <42945386+twito1992@users.noreply.github.com>
2020-05-20 20:30:36 +0300

    new

---
+++ B20_Ex02_02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B20_Ex02_02
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //פה יהיה קישור לאינטרפייס 
+        }
+    }
+}
